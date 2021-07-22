960d05df93a7be9eea41b7b3b3cd27d5f29b4b1a
Nisal <nirvanzentinal@gmail.com>
2021-07-22 16:32:26 +0530

    seo optimised

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,13 @@ function MyApp({ Component, pageProps }) {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#ffc40d" />
+        <meta name="title" content="Matreix TDM" />
+        <meta name="description" content="Cod4 Matrix TDM official gaming clan website" />
+        <meta name="keywords" content="Gaming,TDM,Matrix,cod4,clan,nisalk,gamers,gamer,game,pc game,shooter" />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="nisalk" />
         <meta name="theme-color" content="#ffffff" />
         <title>MATRIX Gaming</title>
       </Head>
